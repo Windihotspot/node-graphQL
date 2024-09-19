9a191711053fa6aa11a70c918b4fbc920fd3b559
mycodedlife <ifiokusanga69@gmail.com>
2024-09-19 13:30:08 +0100

    resolver functions

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './schema.js';
+//db
+import db from './db.js';
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
+
 
 // Server setup
 const server = new ApolloServer({
     //typeDefs -- definitions of types of data
     typeDefs,
     //resolvers
+    resolvers
 })
 
 const {url} = await startStandaloneServer(server, {
