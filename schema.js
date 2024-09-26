001dc259ecf377fabb327728b5f3e1fccb3c69cb
mycodedlife <ifiokusanga69@gmail.com>
2024-09-26 11:55:34 +0100

    mutations(add and delete)

--- schema.js
+++ schema.js
@@ -30,6 +30,15 @@ type Query{
     author(id:ID!):Author
     
 }
+
+type Mutation {
+    addGame(game: AddGameInput!): Game
+    deleteGame(id:ID!): [Game]
+}
+input AddGameInput {
+    title: String!,
+    platform: [String!]!
+}
 `
 
 //int, float,string, boolean, ID
